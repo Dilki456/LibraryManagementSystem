f42ab52228908f6647cb6f7cd7be9927e3fda4f9
rasanga bandara <rasangavidumina9@gmail.com>
2026-08-12 15:45:14 +0530

    committed

--- src/main/java/com/example/book_service/service/BookService.java
+++ src/main/java/com/example/book_service/service/BookService.java
@@ -29,8 +29,8 @@ public class BookService {
     }
 
     // Get Book by ID
-    public Optional<Book> getBookById(@NonNull String id) {
-        return bookRepository.findById(id);
+    public @NonNull Optional<Book> getBookById(@NonNull String id) {
+        return Objects.requireNonNull(bookRepository.findById(id));
     }
 
     // Update Book
